425ce455bae1220f4348863a59e5f07e0bc31867
Tomas Valdivieso <tomas@Tomass-MacBook-Air.local>
2025-02-06 22:50:13 +0100

    change the message to say hello Alexis

--- app/api/twiml/route.ts
+++ app/api/twiml/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export async function POST() {
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Say>Hello! Thank you for testing this application.</Say>
+    <Say>Hello Alexis! Thank you for testing this application!</Say>
 </Response>`
 
   return new NextResponse(twiml, {
